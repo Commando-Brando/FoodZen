0f15dc0fe44c901b995919f6afbfa1028c24e82e
Brando <branmuller@gmail.com>
2021-04-10 21:04:08 -0500

    Added Price Properties file Started on the Shop Scene >made ShopController >made ShopModel >Updated Shop.FXML

---
+++ FoodZen/src/application/controllers/ShopController.java
@@ -0,0 +1,33 @@
+package application.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class ShopController {
+
+    @FXML
+    private Button grainsButton;
+
+    @FXML
+    private Button condimentsButton;
+
+    @FXML
+    private ListView<?> shopListView;
+
+    @FXML
+    private Button drinksButton;
+
+    @FXML
+    private Button snacksButton;
+
+    @FXML
+    private Button produceButton;
+
+    @FXML
+    private Button meatButton;
+
+    @FXML
+    private Button dairyButton;
+
+}
