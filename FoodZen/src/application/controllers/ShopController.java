403d0617838260324578466da7d86042def4e6a8
Brando <branmuller@gmail.com>
2021-04-14 21:26:24 -0500

    cleaned code

--- FoodZen/src/application/controllers/ShopController.java
+++ FoodZen/src/application/controllers/ShopController.java
@@ -80,7 +80,6 @@ public class ShopController implements Initializable{
     
     private ShopModel model;
     
-    
     // loadHelper acts as a middle man to figure out which button was pressed and then call the loader with the button text
     @FXML
     public void loadHelper(ActionEvent event) {
