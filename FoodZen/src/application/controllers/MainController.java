2f60edebf7453ccd96f794a6c7a2d485cebf36ca
thebeasknees <hottsauce0216@gmail.com>
2021-04-21 10:47:16 -0500

    Added buttons Cart, Account, Contact Buttons to HomePage

--- FoodZen/src/application/controllers/MainController.java
+++ FoodZen/src/application/controllers/MainController.java
@@ -22,15 +22,33 @@ public class MainController implements Initializable{
 	@FXML
     private Button shopButton;
 	
+	@FXML
+	private Button cartButton;
+	
+	@FXML
+	private Button accountButton;
+	
+	@FXML
+	private Button contactButton;
+	
 	// initialize initializes base UI elements
-	 @Override
-		public void initialize(URL arg0, ResourceBundle arg1) {
-	    	//System.out.println("hello");
-		}
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+	    //System.out.println("hello");
+	}
     
     @FXML
     void shopLaunch(ActionEvent event) throws IOException {
     	launchActivity = FXMLLoader.load(getClass().getResource("../view/Shop.fxml"));
+    	Scene scene = new Scene (launchActivity,800,600);
+    	Stage Window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+    	Window.setScene(scene);
+    	Window.show();
+    }
+    
+    @FXML
+    void cartLaunch(ActionEvent event) throws Exception {
+    	launchActivity = FXMLLoader.load(getClass().getResource("../view/Cart.fxml"));
     	Scene scene = new Scene (launchActivity,800,600);
     	Stage Window = (Stage) ((Node)event.getSource()).getScene().getWindow();
     	Window.setScene(scene);
