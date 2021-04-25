af260e7316fcc0be9103f9963f1fcf988b306178
Home <Home@DESKTOP-52MNH7G.satx.rr.com>
2021-04-25 02:12:33 -0500

    Added goHome ActionEvent on CartController.java

--- FoodZen/src/application/controllers/CartController.java
+++ FoodZen/src/application/controllers/CartController.java
@@ -1,5 +1,25 @@
 package application.controllers;
 
-public class CartController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+public class CartController {
+	
+	@FXML
+	private AnchorPane mainActivity;
+	
+	// takes in an ActionEvent after home button clicked. resets the pane to the home screen
+    @FXML
+    void goHome(ActionEvent event) throws Exception {
+    	mainActivity = FXMLLoader.load(getClass().getResource("../view/HomePage.fxml"));
+    	Scene scene = new Scene (mainActivity);
+    	Stage Window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+    	Window.setScene(scene);
+    	Window.show();
+    }
 }
