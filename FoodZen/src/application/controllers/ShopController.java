44a835b080c619e09af3033d93f1834a10c53dfa
Brando <branmuller@gmail.com>
2021-04-29 21:54:17 -0500

    Testing for fix

--- FoodZen/src/application/controllers/ShopController.java
+++ FoodZen/src/application/controllers/ShopController.java
@@ -526,33 +526,4 @@ public class ShopController implements Initializable{
 		cartList.getItems().clear();
     	loadCart();
 	}
-    
-    // Alert to provide Contact information
- 	public void contactMe(ActionEvent event) {
- 		Alert contact = new Alert(AlertType.CONFIRMATION,
- 				"Account - account@foodzen.com - 555-265-1774\nManagement - management@foodzen.com - 555-265-9623\nSales - sales@foodzen.com - 555-265-1423\nTechnical Support - techsupport@foodzen.com - 555-265-8876\n");
- 		contact.setTitle("Contact information");
- 		contact.setHeaderText("Contact one of these departments with your Foodzen concerns");
- 		contact.show();
- 	}
- 	
- 	// Launches Account scene
-    @FXML
-    void accountLaunch(ActionEvent event) throws Exception {
-    	mainActivity = FXMLLoader.load(getClass().getResource("../view/Account.fxml"));
-    	Scene scene = new Scene (mainActivity, 800, 600);
-    	Stage Window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-    	Window.setScene(scene);
-    	Window.show();
-    }
-    
-    // Launches Cart scene
-    @FXML
-    void cartLaunch(ActionEvent event) throws Exception {
-    	mainActivity = FXMLLoader.load(getClass().getResource("../view/Cart.fxml"));
-    	Scene scene = new Scene (mainActivity, 800, 600);
-    	Stage Window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-    	Window.setScene(scene);
-    	Window.show();
-    }
 }
